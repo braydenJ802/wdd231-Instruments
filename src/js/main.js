b5a6854effb2a4bb61e7ef525ab020e656254d59
Brayden Jones <jon20015@byui.edu>
2025-11-06 11:17:36 -0700

    Broke the API code into fetching and rendering

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 import { fetchInstruments } from './api.js';
 import { renderInstruments } from './ui.js';
-import { loadFromStorage } from './storage.js';
+//import { loadFromStorage } from './storage.js';
 
 fetchInstruments().then(data => {
   renderInstruments(data);
-  loadFromStorage();
+  //loadFromStorage();
 });
