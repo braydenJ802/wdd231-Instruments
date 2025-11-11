f95ef04ed89232e2b067cc9f99083c3563906e3a
Russell161 <rleggjunior@gmail.com>
2025-11-11 10:54:05 -0700

    added correct vite config

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        signup: resolve(__dirname, "src/signup.html"),
+        signup: resolve(__dirname, "src/signup.html")
       }
     }
   }
