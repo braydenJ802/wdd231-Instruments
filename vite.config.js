ca87524ce5e5f17c5b5786c8d99955ee876a05cb
Russell161 <rleggjunior@gmail.com>
2025-11-11 10:51:02 -0700

    added to vite fl

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html")
+        main: resolve(__dirname, "src/index.html"),
+        signup: resolve(__dirname, "src/signup.html"),
       }
     }
   }
